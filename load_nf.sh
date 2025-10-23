1547b0b2a9c06d6b9bdaafb2d98c52e996ee0f0b
Ryan Gorzek <ryan.gorzek@icloud.com>
2025-10-23 10:42:41 -0700

    Enhancement: cache conda environments for STAR processes (resolves #2)

--- load_nf.sh
+++ load_nf.sh
@@ -7,5 +7,7 @@ module load anaconda3
 export NXF_OPTS='-Xms256m -Xmx1g -Xss256k -XX:+UseSerialGC -XX:ActiveProcessorCount=8'
 export NXF_DEFAULT_CPUS=2
 export NXF_DEFAULT_MEMORY='10 GB'
+mkdir -p "$SCRATCH/.nxf-conda"
+export NXF_CONDA_CACHEDIR="$SCRATCH/.nxf-conda"
 
 nextflow -version
